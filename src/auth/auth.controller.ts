5dc5dcf48ce76817783c37ec5a5cffa5048fe3ae
Ivan Rojo <ivanrojo07@gmail.com>
2025-12-18 16:58:28 -0600

    Auth Guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './guard/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +16,10 @@ export class AuthController {
     const user = this.authService.login(loginDto);
     return user;
     // return this.authService.login();
+  }
+  @UseGuards(AuthGuard)
+  @Get('/is_valid_token')
+  isValidToken() {
+    return { message: 'Token is valid' };
   }
 }
